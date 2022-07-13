2d077d524c41b7f3f825d8b2caca37c0ff270bf6
Joseph Flinn <joseph.s.flinn@gmail.com>
2022-07-13 11:24:22 -0700

    adding a docker service install to the framework laptop

--- machines/framework.nix
+++ machines/framework.nix
@@ -30,6 +30,8 @@
   #networking.interfaces.enp0s13f0u4u2.useDHCP = true;
   #networking.interfaces.enp0s13f0u4u3.useDHCP = true;
 
+  virtualisation.docker.enable = true;
+
   users.users.joseph = {
     isNormalUser = true;
     extraGroups = [ "wheel" "dialout" "video" "docker" ];
@@ -55,6 +57,6 @@
   # this value at the release version of the first install of this system.
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "21.11"; # Did you read the comment?
+  system.stateVersion = "22.05"; # Did you read the comment?
 
 }
